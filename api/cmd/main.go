563ef90430d1824563eef19990b898cc2aafc9cd
Gierdiaz <gierdiaz@hotmail.com>
2024-07-27 23:05:54 -0300

    feat: server running

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/Gierdiaz/GoStock/api/internal/database"
+	"github.com/Gierdiaz/GoStock/api/internal/router"
+)
+
 func main() {
 
-	//
+	database.ConnectionDatabase()
+
+	r := router.SetupRouter()
+	fmt.Println("Server running on port 8080")
+	err := r.Run(":8080")
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
